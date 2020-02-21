a517a0e7306fdd622125ce190de63def19e34892
hua7328 <hua7328@126.com>
2020-02-21 11:57:38 +0800

    Revert "Revert "Revert "55555555"""
    This reverts commit 747cf4b8b110e18ec0b8f1357268a173047199cc.

--- src/P_SY7_Head1.java
+++ src/P_SY7_Head1.java
@@ -1,5 +1,5 @@
-﻿package fywzqx.inst.sy7;
-4444444444444444444444444444444444444444444444444444444444
+package fywzqx.inst.sy7;
+
 package fywzqx.inst.sy71;
 package fywzqx.inst.sy72;
 package fywzqx.inst.sy73;
